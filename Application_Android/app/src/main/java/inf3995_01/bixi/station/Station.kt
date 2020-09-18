c616a7ac8d687ade025ce42125cc76bc7c08917d
Talla <cyfongang@yahoo.fr>
2020-09-18 19:47:24 -0400

    [Cyrille-Robin] delete old android project

--- Application_Android/app/src/main/java/inf3995_01/bixi/station/Station.kt
+++
@@ -1,9 +0,0 @@
-package inf3995_01.bixi.station
-
-data class Station (var id:Long,
-                    var code: Int,
-                    var name: String,
-                    var latitude:String,
-                    var longitude:String)  {
-
-}
