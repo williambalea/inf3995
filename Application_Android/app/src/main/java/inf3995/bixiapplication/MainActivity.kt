15ad8b2840a3435b9210c65578ddc64f9f84f216
Talla <cyfongang@yahoo.fr>
2020-09-19 01:04:46 -0400

    [Cyrille-Robin] Android talking with httpbin.org server

--- Application_Android/app/src/main/java/inf3995/bixiapplication/MainActivity.kt
+++ Application_Android/app/src/main/java/inf3995/bixiapplication/MainActivity.kt
@@ -8,41 +8,62 @@ import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+//import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.converter.scalars.ScalarsConverterFactory
 
 
 class MainActivity : AppCompatActivity() {
 
-    val TAG ="MainActivity"
+    val TAG = "MainActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        // STRING
         val retrofit = Retrofit.Builder()
             .baseUrl("http://httpbin.org")
             .addConverterFactory(ScalarsConverterFactory.create())
             .build()
 
-        val service:HttpWebServicesString =retrofit.create(HttpWebServicesString::class.java)
+        val service:HttpWebServicesString = retrofit.create(HttpWebServicesString::class.java)
         val call:Call<String> = service.getUserAgent()
-        //val call2:Call<String> = service.getUserIP()
+        val call2:Call<String> = service.getUserIP()
         call.enqueue(object: Callback<String> {
-            override fun onResponse(call: Call<String>, response: Response<String>) {
-                Log.i(TAG,"User agent response: ${response.body()}")
+            override fun onResponse(call: Call<String>?, response: Response<String>?) {
+                Log.i(TAG,"User agent response: ${response?.body()}")
             }
-            override fun onFailure(call: Call<String>, t: Throwable) {
-
+            override fun onFailure(call: Call<String>?, t: Throwable) {
             }
         })
 
-       // call2.enqueue(object: Callback<String> {
-       //     override fun onResponse(call2: Call<String>, response: Response<String>) {
-        //        Log.i(TAG,"User IP response: ${response?.body()}")
-       //     }
-       //     override fun onFailure(call2: Call<String>, t: Throwable) {
+       call2.enqueue(object: Callback<String> {
+           override fun onResponse(call2: Call<String>?, response: Response<String>?) {
+                Log.i(TAG,"User IP response: ${response?.body()}")
+           }
+           override fun onFailure(call2: Call<String>, t: Throwable) {
+           }
+        })
 
-       //     }
-      //  })
+        // JSON
+        val retrofitJson = Retrofit.Builder()
+            .baseUrl("http://httpbin.org")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        val serviceJson = retrofitJson.create(HttpWebServicesJson::class.java)
+        val callJson = serviceJson.getUserInfo()
+        callJson.enqueue(object:Callback<GetData> {
+
+            override fun onResponse(call: Call<GetData>?, response: Response<GetData>?) {
+                val getData= response?.body()
+                Log.i(TAG, "Received url: ${getData}")
+            }
+            override fun onFailure(call: Call<GetData>, t: Throwable) {
+                TODO("Not yet implemented")
+            }
+
+
+        })
     }
 
 }
