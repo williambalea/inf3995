581a0cde2e59b69e5410ffc9ac50eecea109df8f
Dalyna Pak <dalyna.pak@hotmail.fr>
2020-09-20 11:08:37 -0400

    test hello world in server
    Signed-off-by: Dalyna Pak <dalyna.pak@hotmail.fr>

--- hello.cc
+++ hello.cc
@@ -1,16 +1,24 @@
 
-#include "./pistache/"
+#include "pistache/endpoint.h"
 
 using namespace Pistache;
 using namespace Net;
 
 
 struct HelloHandler : public Http::Handler {
+public:
+    HTTP_PROTOTYPE(HelloHandler)
     void onRequest(const Http::Request& req, Http::ResponseWriter response) {
         response.send(Http::Code::Ok, "Hello, World");
     }
 };
 
 int main() {
-    Http::listenAndServe<HelloHandler>("*:9080");
+    Address addr(Ipv4::any(), Port(9082));
+    auto opts = Http::Endpoint::options().threads(1);
+    Http::Endpoint server(addr);
+    server.init(opts);
+    server.setHandler(Http::make_handler<HelloHandler>());
+    server.serve();
+    server.shutdown();
 }
