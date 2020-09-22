e2b725198fd7845cb944cc79ef9eab709d8d1c2e
dalyz <dalyna.pak@hotmail.fr>
2020-09-22 10:32:36 -0700

    [Dalyna]: add android communication with server (Post)
    Signed-off-by: dalyz <dalyna.pak@hotmail.fr>

--- server.cpp
+++ server.cpp
@@ -1,15 +1,22 @@
 #include "pistache/endpoint.h"
 
 using namespace Pistache;
+using namespace Http;
 
 class HelloHandler : public Http::Handler {
 public:
 
     HTTP_PROTOTYPE(HelloHandler)
 
-    void onRequest(const Http::Request& request, Http::ResponseWriter response) {
-        response.send(Http::Code::Ok, "Hello World");
-    }
+    void onRequest(const Http::Request& req, Http::ResponseWriter response) override {
+            if (req.resource() == "/ip_server") {
+                if(req.method() == Http::Method::Post) {
+                    response.send(Http::Code::Ok, "Server received IP:" + req.body() + " " + "from Android");
+                }
+            } else {
+                response.send(Http::Code::Ok, "Hello World From Server");
+            }
+        }
 };
 
 int main() {
@@ -20,6 +27,5 @@ int main() {
     server.init(opts);
     server.setHandler(Http::make_handler<HelloHandler>());
     server.serve();
-
     server.shutdown();
 }
