c06b27da0c2cf2e2c66a74f2bd0d3cb04bd1d628
williambalea <william.balea@polymtl.ca>
2020-10-07 02:00:28 -0400

    [William] Fixed db always reset bug and clean up

--- CentralServer/mysql.hpp
+++ CentralServer/mysql.hpp
@@ -17,64 +17,72 @@ using namespace sql::mysql;
 
 class MySQL {
 public:
-    MySQL() {
-        driver = get_mysql_driver_instance();
-        setupServerDB();
-    }
-
-    void sendPoll(std::string email, std::string firstName, std::string lastName, int age, bool interest) {
-        connectToServerDB();
-        
-        PreparedStatement* stmt = con->prepareStatement(
-            "INSERT INTO Polls (email, firstName, lastName, age, interest) VALUES (?, ?, ?, ?, ?)");
-
-        stmt->setString(1, email);
-        stmt->setString(2, firstName);
-        stmt->setString(3, lastName);
-        stmt->setInt(4, age);
-        stmt->setBoolean(5, interest);
-        stmt->execute();
-
-        delete stmt;
-        disconnectFromServerDB();
-    }
+    MySQL();
+    void sendPoll(std::string email, std::string firstName, std::string lastName, int age, bool interest);
 
 private:
-    void connectToServerDB() {
-        con = driver->connect(HOST, USER, PASS);
-        Statement* stmt = con->createStatement();
-        stmt->execute("USE Server");
-        delete stmt;
-    }
-
-    void disconnectFromServerDB() {
-        delete con;
-    }
-
-    void setupServerDB() {
-        con = driver->connect(HOST, USER, PASS);
-        Statement* stmt = con->createStatement();
-
-        stmt->execute("DROP DATABASE IF EXISTS Server");
-        stmt->execute("CREATE DATABASE Server");
-        stmt->execute("USE Server");
-        stmt->execute("DROP TABLE IF EXISTS Polls");
-        // TODO : ask android app how they manage string verification
-        stmt->execute(
-            "CREATE TABLE Polls ("
-            "email VARCHAR(40), "
-            "firstName VARCHAR(40), "
-            "lastName VARCHAR(40), "
-            "age TINYINT, "
-            "interest BOOL)"
-        );
-
-        delete stmt;
-        delete con;
-    }
+    void connect();
+    void disconnect();
+    void setup();
 
     MySQL_Driver *driver;
     Connection *con;
 };
 
-#endif
+
+MySQL::MySQL() {
+    driver = get_mysql_driver_instance();
+    setup();
+}
+
+void MySQL::sendPoll(std::string email, std::string firstName, std::string lastName, int age, bool interest) {
+    connect();
+    
+    PreparedStatement* stmt = con->prepareStatement(
+        "INSERT INTO Polls (email, firstName, lastName, age, interest) VALUES (?, ?, ?, ?, ?)"
+    );
+
+    stmt->setString(1, email);
+    stmt->setString(2, firstName);
+    stmt->setString(3, lastName);
+    stmt->setInt(4, age);
+    stmt->setBoolean(5, interest);
+    stmt->execute();
+
+    delete stmt;
+    disconnect();
+}
+
+void MySQL::connect() {
+    con = driver->connect(HOST, USER, PASS);
+    Statement* stmt = con->createStatement();
+    stmt->execute("USE Server");
+    delete stmt;
+}
+
+void MySQL::disconnect() {
+    delete con;
+}
+
+void MySQL::setup() {
+    con = driver->connect(HOST, USER, PASS);
+    Statement* stmt = con->createStatement();
+
+    stmt->execute("DROP DATABASE IF EXISTS Server");
+    stmt->execute("CREATE DATABASE Server");
+    stmt->execute("USE Server");
+    stmt->execute("DROP TABLE IF EXISTS Polls");
+    stmt->execute(
+        "CREATE TABLE Polls ("
+        "email VARCHAR(40), "
+        "firstName VARCHAR(40), "
+        "lastName VARCHAR(40), "
+        "age TINYINT, "
+        "interest BOOL)"
+    );
+
+    delete stmt;
+    delete con;
+}
+
+#endif // MYSQL_HPP
