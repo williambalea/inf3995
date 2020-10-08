a88c9873ec7b34b994ddbb3217a465c9ae3b2c25
jmlasnier-ubuntu <j-m.lasnier@hotmail.com>
2020-10-08 15:52:35 -0400

    [JML] able to fetch data from BD and convert to JSON format

--- enginsDonnees/mySql.py
+++ enginsDonnees/mySql.py
@@ -1,5 +1,5 @@
 import mysql.connector
-
+import json
 from mysql.connector import Error
 
 HOST_NAME = "34.70.117.28"
@@ -7,6 +7,7 @@ USER_NAME = "root"
 USER_PASSWORD = "jerome"
 MY_DATABASE = "Bixi"
 SQL_REQ1 = "SELECT * FROM Stations"
+CODE_STATION = "5003"
 
 def create_connection(host_name, user_name, user_password):
     connection = None
@@ -22,14 +23,22 @@ def create_connection(host_name, user_name, user_password):
         print(f"The error '{e}' occurred")
     return connection
 
-connection = create_connection(HOST_NAME, USER_NAME, USER_PASSWORD)
 
-def getStationCode(code):
+def getStationCode(allo):
     mycursor = connection.cursor()
-    mycursor.execute(SQL_REQ1)
-    myresult = mycursor.fetchone()
-    RETUprint(myresult) 
-    return(0)
+    mycursor.execute("SELECT S.name, S.latitude, S.longitude FROM Stations S WHERE code='{}'".format(allo))
+    row_headers=[x[0] for x in mycursor.description] #extract row headers
+    myresult = mycursor.fetchall()
+    json_data=[]
+    for result in myresult:
+        json_data.append(dict(zip(row_headers,result)))
+    return json.dumps(json_data)
+
+connection = create_connection(HOST_NAME, USER_NAME, USER_PASSWORD)
+station5003 = getStationCode(CODE_STATION)
+
+
+print(station5003)
 
 
 
