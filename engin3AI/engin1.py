b0090316d0c065d4f891441c7bbdf6a3b5481580
William <williambalea27@gmail.com>
2020-10-11 04:25:23 -0400

    [Willam] Exposed port 5000 and changed route to /engin1

--- engin3AI/engin1.py
+++ engin3AI/engin1.py
@@ -2,10 +2,8 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/engin1')
 def hello_world():
     return 'Hello, World from Engin1!'
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
     
