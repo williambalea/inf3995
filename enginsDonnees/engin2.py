8977d0bd5c6831cad619f7a5932bcc6599350f07
Jean-Michel Lasnier <j-m.lasnier@hotmail.com>
2020-10-17 13:04:16 -0700

    [JML] graph usage per hour for a specific station DONE!

--- enginsDonnees/engin2.py
+++ enginsDonnees/engin2.py
@@ -1,11 +1,45 @@
-import mysql.connector
-from mysql.connector import Error
 import json
-from matplotlib import pyplot as plt
+from enginSQL import EnginSQL
 import pandas as pd
-from flask.json import jsonify
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
 
 class Engin2:
 
+    def __init__(self):
+        return None
     
+    def getPerHourCountStart(self, df):
+        df['startDate'] = pd.to_datetime(df['startDate'])
+        startDateSeries = df['startDate'].values.astype('datetime64[m]')
+        hours = np.remainder(startDateSeries.astype("M8[h]").astype("int"), 24)
+        hourCount = np.bincount(hours)
+        # return dict(zip(range(24), hourCount))
+        return hourCount
+    
+    def getPerHourCountEnd(self,df):
+        df['endDate'] = pd.to_datetime(df['endDate'])
+        startDateSeries = df['endDate'].values.astype('datetime64[m]')
+        hours = np.remainder(startDateSeries.astype("M8[h]").astype("int"), 24)
+        hourCount = np.bincount(hours)
+        # return dict(zip(range(24), counts))
+        return hourCount
+
+    def getPerWeekDayCount(self, df):
+        df['startDate'] = pd.to_datetime(df['startDate'])
+        startDateSeries = df['startDate'].values.astype('datetime64[m]')
+        #0=thursday, 2=saturday, 4=monday, 6=wednesday
+        #with +4, 0=sunday, 2=tuesday, 5=friday, 6-saturday 
+        days = np.remainder(startDateSeries.astype("M8[D]").astype("int")+4, 7)
+        dayCount = np.bincount(days)
+        # return dict(zip(range(24), counts))
+        return dayCount
+
+    def getPerMonthCount(self, df):
+        df['startDate'] = pd.to_datetime(df['startDate'])
+        startDateSeries = df['startDate'].values.astype('datetime64[m]')
+        months = np.remainder(startDateSeries.astype("M8[M]").astype("int"), 12)
+        monthCount = np.bincount(months)
+        return monthCount
 
--- enginsDonnees/lab.py
+++ enginsDonnees/lab.py
@@ -4,6 +4,7 @@ import json
 import socket
 import requests as rq
 from enginSQL import EnginSQL
+from engin2 import Engin2
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
@@ -18,19 +19,13 @@ CODE_STATION = "5003"
 URL_POST_TEMP = "/serveur/code"
 PORT = 2000
 
-
+colors = ["#006D2C", "#31A354", "#74C476"]
 
 enginsql = EnginSQL()
+engin2 = Engin2()
 
 station = 'toutes'
 year = 2015
-weekDays = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
-months = [ 'April', 'May', 'June', 'July','August',
-            'September', 'October', 'November']
-monthsNumber = [ '4/', '5/', '6/', '7/', '8/',
-                 '9/', '10/', '11/']
-columnNames = ['id', 'startDate', 'startStationCode', 'endDate', 'endStationCode', 'durationSec', 'isMember']
-count24h = np.zeros(24)
 
 
 #create dataframe
@@ -41,44 +36,46 @@ df = enginsql.jsonToPandas(amijson)
 df['endDate'] = pd.to_datetime(df['endDate'])
 
 
-def getPerHourCountEnd(dataframe):
-    dataframe['endDate'] = pd.to_datetime(dataframe['endDate'])
-    startDateSeries = dataframe['endDate'].values.astype('datetime64[m]')
-    hours = np.remainder(startDateSeries.astype("M8[h]").astype("int"), 24)
-    hourCount = np.bincount(hours)
-    # return dict(zip(range(24), counts))
-    return hourCount
 
-def getPerWeekDayCount(dataframe):
-    dataframe['startDate'] = pd.to_datetime(dataframe['startDate'])
-    startDateSeries = dataframe['startDate'].values.astype('datetime64[m]')
-    #0=thursday, 2=saturday, 4=monday, 6=wednesday
-    #with +4, 0=sunday, 2=tuesday, 5=friday, 6-saturday 
-    days = np.remainder(startDateSeries.astype("M8[D]").astype("int")+4, 7)
-    dayCount = np.bincount(days)
-    # return dict(zip(range(24), counts))
-    return dayCount
+print('Par heures:')
+hourCountStart = engin2.getPerHourCountStart(df)
+hourCountEnd = engin2.getPerHourCountEnd(df)
+# print(hourCountEnd)
+# print(hourCountStart)
+# new = np.stack((hourCountStart, hourCountEnd), axis=1)
+# print("new: ")
+# print(new)
+# sns.displot(list(new), multiple="stack", rug=True)
+# plt.show()
 
-def getPerMonthCount(dataframe):
-    dataframe['startDate'] = pd.to_datetime(dataframe['startDate'])
-    startDateSeries = dataframe['startDate'].values.astype('datetime64[m]')
-    months = np.remainder(startDateSeries.astype("M8[M]").astype("int"), 12)
-    monthCount = np.bincount(months)
-    return monthCount
-
-def getPerHourCountStart(dataframe):
-    dataframe['startDate'] = pd.to_datetime(dataframe['startDate'])
-    startDateSeries = dataframe['startDate'].values.astype('datetime64[m]')
-    hours = np.remainder(startDateSeries.astype("M8[h]").astype("int"), 24)
-    hourCount = np.bincount(hours)
-    # return dict(zip(range(24), hourCount))
-    return hourCount
-print('Par mois:')
-print(getPerMonthCount(df))
-print('Par jour de la semaine:')
-print(getPerWeekDayCount(df))
-print('Par heure:')
-print(getPerHourCountStart(df))
+# set width of bar
+barWidth = 0.25
+ 
+# set height of bar
+bars1 = [12, 30, 1, 8, 22]
+bars2 = [28, 6, 16, 5, 10]
+bars3 = [29, 3, 24, 25, 17]
+ 
+# Set position of bar on X axis
+r1 = np.arange(len(hourCountStart))
+r2 = [x + barWidth for x in r1]
+ 
+# Make the plot
+plt.bar(r1, hourCountStart, color='#0A6BF3', width=barWidth, edgecolor='white', label='Start')
+plt.bar(r2, hourCountEnd, color='#F3380A', width=barWidth, edgecolor='white', label='End')
+ 
+# Add xticks on the middle of the group bars
+plt.xlabel('Per Hour', fontweight='bold')
+plt.ylabel('User Count', fontweight='bold')
+plt.xticks([r + barWidth for r in range(len(hourCountStart))], ['0h', '1h', '02', '03', '04', '05','06','07','08','09','10','11','12','13','14','15','16','17','18','19','20','21','22','23'])
+ 
+# Create legend & Show graphic
+plt.legend()
+plt.show()
+# print('Par jour de la semaine:')
+# print(getPerWeekDayCount(df))
+# print('Par heure:')
+# print(getPerHourCountStart(df))
 
 
 # sns.displot(list(startCount), list(endCount),  multiple="stack", rug=True)
