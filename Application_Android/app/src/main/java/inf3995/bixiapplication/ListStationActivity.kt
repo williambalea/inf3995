ba7b43298114f492e09c73fdf765589e9f46fe34
Robin Kuchue <kuchue.kamgain-djoko@polymtl.ca>
2020-10-19 17:02:43 -0400

    [Robin] progressBar implementation

--- Application_Android/app/src/main/java/inf3995/bixiapplication/ListStationActivity.kt
+++ Application_Android/app/src/main/java/inf3995/bixiapplication/ListStationActivity.kt
@@ -3,6 +3,7 @@ package inf3995.bixiapplication
 import android.os.Bundle
 import android.util.Log
 import android.view.Menu
+import android.view.View
 import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -20,31 +21,6 @@ import retrofit2.converter.scalars.ScalarsConverterFactory
 
 class ListStationActivity : AppCompatActivity(){ //StationAdapter.ClickedItem
 
-        /*
-        val ipAddress = "70.80.27.156"
-        val ipAddress = "135.19.27.218"
-        val port = "2000"
-        val port = "2001"
-        var itemListModal = arrayListOf(
-            Station(123, "Cote vertu Est", 11.244456F, 21.076599F),
-            Station(452, "Langelier Est", 12.374400F, 22.356466F),
-            Station(375, "Montreal Nord", 13.365400F, 23.000466F),
-            Station(458, "Polytechnique", 14.321400F, 24.348466F),
-            Station(589, "Montréal", 15.374980F, 25.356466F),
-            Station(600, "Saint Laurent", 16.3744180F, 26.986466F),
-            Station(987, "Saint michel", 17.365400F, 27.000466F),
-            Station(876, "Cote des neiges", 18.321400F, 28.348466F),
-            Station(239, "Centre ville", 49.374980F, 29.356466F),
-            Station(376, "Montreal Ouest", 53.365400F, 23.000466F),
-            Station(444, "ETS", 14.321400F, 24.348466F),
-            Station(580, "Saint_constant", 35.374980F, 25.356466F),
-            Station(600, "Bonaventure", 16.3744180F, 26.986466F),
-            Station(917, "Boulevard Lavesque", 47.365400F, 27.000466F),
-            Station(874, "Cote sainte catherine", 18.321400F, 28.348466F),
-            Station(289, "Cote de Liesse", 19.374980F, 49.356466F),
-            Station(120, "Avenue papineau", 20.3744180F, 30.986466F))
-        */
-
     private val TAG = "Stations List"
     var stationModalList = ArrayList<Station>()
     var stationAdapter: StationAdapter? = null
@@ -56,10 +32,7 @@ class ListStationActivity : AppCompatActivity(){ //StationAdapter.ClickedItem
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.station_list_activity_main)
-
         requestToServer(IpAddressDialog.ipAddressInput, IpAddressDialog.portInput)
-        requestConnectionWithServer(IpAddressDialog.ipAddressInput, IpAddressDialog.portInput)
-
     }
 
 
@@ -110,6 +83,7 @@ class ListStationActivity : AppCompatActivity(){ //StationAdapter.ClickedItem
                 val arrayStationType = object : TypeToken<Array<Station>>() {}.type
                 val jObj: Array<Station> = Gson().fromJson(response?.body(), arrayStationType)
                 makeRecyclerView(jObj)
+                llProgressBar.visibility = View.GONE
             }
             override fun onFailure(call: Call<String>?, t: Throwable) {
                 Log.i(TAG, "Echec de connexion avec le serveur !!!")
@@ -128,26 +102,6 @@ class ListStationActivity : AppCompatActivity(){ //StationAdapter.ClickedItem
 
         recyclerView.layoutManager = LinearLayoutManager(this@ListStationActivity)
         recyclerView.setHasFixedSize(true)
-    }
-
-    private fun requestConnectionWithServer(ipAddresss:String, ports:String) {
-
-        // get Server Ip adress
-        val retrofit2 = Retrofit.Builder()
-            .baseUrl("http://$ipAddresss:$ports/")
-            .addConverterFactory(ScalarsConverterFactory.create())
-            .build()
-
-        val service2: WebBixiService = retrofit2.create(WebBixiService::class.java)
-        val call2: Call<String> = service2.getHelloWorld()
-
-        call2.enqueue(object : Callback<String> {
-            override fun onResponse(call: Call<String>?, response: Response<String>?) {
-                Log.i(TAG, "Réponse 2 du Serveur: ${response?.body()}")
-            }
-            override fun onFailure(call: Call<String>?, t: Throwable) {
-            }
-        })
     }
 
 }
