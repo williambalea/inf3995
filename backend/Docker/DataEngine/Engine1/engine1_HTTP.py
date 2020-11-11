160b6ca9f23b3b1d008438b37cb5108b1785df18
Dalyna Pak <dalyna.pak@hotmail.fr>
2020-11-11 11:37:11 -0800

    [Dalyna]: refactor engine1 in new branch
    Signed-off-by: Dalyna Pak <dalyna.pak@hotmail.fr>

--- backend/Docker/DataEngine/Engine1/engine1_HTTP.py
+++ backend/Docker/DataEngine/Engine1/engine1_HTTP.py
@@ -1,18 +1,18 @@
 from flask import Flask
 from engine1 import Engine1
+import logging
 
 app = Flask(__name__)
-
 engine1 = Engine1()
 
 @app.route('/engine1')
 def hello_world():
     return 'Hello World from Engin 1'
-
-@app.route('/engine1/station/<code>')
-def stationCode(code):
-    return engine1.getStationCode(code)
     
 @app.route('/engine1/station/all')
 def allStation():
     return engine1.getAllStations() 
+
+@app.route('/engine1/logs/<octet>')
+def logs(octet):
+    return engine1.logsToJSON(octet)
