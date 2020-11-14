9b317ea39251b1d0e4e67e2c7b194319de3cbe04
Jean-Michel Lasnier <j-m.lasnier@hotmail.com>
2020-11-14 16:12:10 -0500

    [JML] big dataframe for training done for large RAM only

--- backend/Docker/DataEngine/Engine3/engine3.py
+++ backend/Docker/DataEngine/Engine3/engine3.py
@@ -7,8 +7,53 @@ import base64
 
 class Engine3:
 
+    # csv_file_list = ["../kaggleData/OD_2014.csv","../kaggleData/OD_2015.csv", "../kaggleData/OD_2016.csv"]
 
+    csv_file_list = ["../kaggleData/OD_2015.csv", "../kaggleData/OD_2016.csv"]
 
     def __init__(self):
         return None 
     
+    def importBixiData(self):
+        # csv_file_list = ["../kaggleData/OD_2014.csv", "../kaggleData/OD_2015.csv","../kaggleData/OD_2016.csv"]sv"]
+        list_of_dataframes = []
+        for filename in self.csv_file_list:
+            list_of_dataframes.append(pd.read_csv(filename, dtype={'start_date':str,'start_station_code':int,'end_date':str,
+                                                                'end_station_code':str,'duration_sec':int,'is_member':int})  )
+        return list_of_dataframes
+
+    def convertBixiDataToDatetime(self, list_df):
+        list_df[0]['start_date'] = pd.to_datetime(list_df[0]['start_date'])
+        list_df[0]['end_date'] = pd.to_datetime(list_df[0]['end_date'])
+        list_df[1]['start_date'] = pd.to_datetime(list_df[1]['start_date'])
+        list_df[1]['end_date'] = pd.to_datetime(list_df[1]['end_date'])
+        # list_df[2]['start_date'] = pd.to_datetime(list_df[2]['start_date'])
+        # list_df[2]['end_date'] = pd.to_datetime(list_df[2]['end_date'])
+        return list_df
+
+    def get_weather_df(self):
+        print('importing weather data')
+        weather_description = pd.read_csv('../kaggleData/historical-hourly-weather-data/weather_description.csv', low_memory=False)
+        temperature = pd.read_csv('../kaggleData/historical-hourly-weather-data/temperature.csv', low_memory=False)
+        pressure = pd.read_csv('../kaggleData/historical-hourly-weather-data/pressure.csv', low_memory=False)
+        wind_speed = pd.read_csv('../kaggleData/historical-hourly-weather-data/wind_speed.csv', low_memory=False)
+
+        print(weather_description.shape)
+        print('filter only montreal')
+        weather_description = weather_description.filter(items=['datetime','Montreal'])
+        temperature = temperature.filter(items=['datetime','Montreal']) #temp in  Kelvin
+        pressure = pressure.filter(items=['datetime','Montreal'])
+        wind_speed = wind_speed.filter(items=['datetime','Montreal'])
+
+        weather = (weather_description.merge(temperature, on='datetime').
+                                    merge(pressure, on='datetime').
+                                    merge(wind_speed, on='datetime'))
+        weather.columns = ['Datetime','Description', 'Temperature', 'Pressure', 'Wind_speed']
+        weather['Temperature_C'] = weather['Temperature'] - 273.15 #Añadimos columna de temperatura en grados centígrados
+
+        weather['Datetime'] = pd.to_datetime(weather['Datetime'])
+        weather = weather.sort_values(by = ['Datetime'])
+        print('Weather_df done')
+        print(weather.shape)
+        return weather
+
--- backend/Docker/DataEngine/Engine3/lab3.py
+++ backend/Docker/DataEngine/Engine3/lab3.py
@@ -3,7 +3,8 @@ from mysql.connector import Error
 import json
 import socket
 import requests as rq
-# from Engine3.engine3 import Engine3
+from engine3 import Engine3
+# from engine2 import Engine2
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
@@ -24,7 +25,6 @@ colors = ["#006D2C", "#31A354", "#74C476"]
 # print('creating enginsql')
 # engine1 = Engine1()
 # print('creating engine3')
-# engine3 = Engine3()
 
 stationToutes = 'all'
 station6202 = 6202
@@ -45,61 +45,131 @@ timeMonth = "perMonth"
 
 # print('reading od2015')
 
-
+print('constructing engine3')
+engine3 = Engine3()
 
 # #Import Data
 print('importing bixi csv data')
 # csv_file_list = ["../kaggleData/OD_2014.csv", "../kaggleData/OD_2015.csv","../kaggleData/OD_2016.csv"]
-csv_file_list = ["../kaggleData/OD_2014.csv"]
-list_of_dataframes = []
-for filename in csv_file_list:
-    list_of_dataframes.append(pd.read_csv(filename, dtype={'start_date':str,'start_station_code':int,'end_date':str,
- 
-                                                           'end_station_code':str,'duration_sec':int,'is_member':int})  )
+bixidf2014 = pd.read_csv("../kaggleData/OD_2014.csv", dtype={'start_date':str,'start_station_code':int,'end_date':str,
+                                                        'end_station_code':str,'duration_sec':int,'is_member':int})
+bixidf2015 = pd.read_csv("../kaggleData/OD_2015.csv", dtype={'start_date':str,'start_station_code':int,'end_date':str,
+                                                        'end_station_code':str,'duration_sec':int,'is_member':int})
+bixidf2016 = pd.read_csv("../kaggleData/OD_2016.csv", dtype={'start_date':str,'start_station_code':int,'end_date':str,
+                                                        'end_station_code':str,'duration_sec':int,'is_member':int})
+# csv_file_list = ["../kaggleData/OD_2014.csv","../kaggleData/OD_2015.csv", "../kaggleData/OD_2016.csv"]
+# list_of_dataframes = []
+# for filename in csv_file_list:
+#     list_of_dataframes.append(pd.read_csv(filename, dtype={'start_date':str,'start_station_code':int,'end_date':str,
+#                                                          'end_station_code':str,'duration_sec':int,'is_member':int})  )
+# list_of_dataframes = []
+# list_of_dataframes = engine3.importBixiData()
+
 print('concatenation bixi data')
-merged_df = pd.concat(list_of_dataframes)
+# merged_df = pd.concat(list_of_dataframes)
+# print(list_of_dataframes[0].shape)
+# print(list_of_dataframes[1].shape)
+# print(list_of_dataframes[2].shape)
 # print('shape of merge')
 # print(merged_df.shape)
 
-print('importing weather data')
-weather_description = pd.read_csv('../kaggleData/historical-hourly-weather-data/weather_description.csv', low_memory=False)
-temperature = pd.read_csv('../kaggleData/historical-hourly-weather-data/temperature.csv', low_memory=False)
-pressure = pd.read_csv('../kaggleData/historical-hourly-weather-data/pressure.csv', low_memory=False)
-wind_speed = pd.read_csv('../kaggleData/historical-hourly-weather-data/wind_speed.csv', low_memory=False)
 
 # convert bixi data dateTime
+# print('converting bixi data to datetiime')
+# merged_df['start_date'] = pd.to_datetime(merged_df['start_date'])
+# merged_df['end_date'] = pd.to_datetime(merged_df['end_date'])
+# merged_df[['start_date', 'end_date']].dtypes
+# print('bixiData to Pandas OK' )
 print('converting bixi data to datetiime')
-merged_df['start_date'] = pd.to_datetime(merged_df['start_date'])
-merged_df['end_date'] = pd.to_datetime(merged_df['end_date'])
-merged_df[['start_date', 'end_date']].dtypes
+bixidf2014['start_date'] = pd.to_datetime(bixidf2014['start_date'])
+bixidf2014['end_date'] = pd.to_datetime(bixidf2014['end_date'])
+bixidf2015['start_date'] = pd.to_datetime(bixidf2015['start_date'])
+bixidf2015['end_date'] = pd.to_datetime(bixidf2015['end_date'])
+bixidf2016['start_date'] = pd.to_datetime(bixidf2016['start_date'])
+bixidf2016['end_date'] = pd.to_datetime(bixidf2016['end_date'])
+# list_of_dataframes[0][['start_date', 'end_date']].dtypes
+# list_of_dataframes = engine3.convertBixiDataToDatetime(list_of_dataframes)
 print('bixiData to Pandas OK')
 
 
-#filter and manipulate weather
-print('filter only montreal')
-weather_description = weather_description.filter(items=['datetime','Montreal'])
-temperature = temperature.filter(items=['datetime','Montreal']) #temp in  Kelvin
-pressure = pressure.filter(items=['datetime','Montreal'])
-wind_speed = wind_speed.filter(items=['datetime','Montreal'])
-print(weather_description.shape)
+weather_df = engine3.get_weather_df()
 
-#merge data by datetime
-print('merging all weather data')
-weather = (weather_description.merge(temperature, on='datetime').
-                              merge(pressure, on='datetime').
-                              merge(wind_speed, on='datetime'))
-weather.columns = ['Datetime','Description', 'Temperature', 'Pressure', 'Wind_speed']
-weather['Temperature_C'] = weather['Temperature'] - 273.15 #to adjust to Celcius
+# #now include weather data with bixi data based on datetime
+# print('df_complete 1 sorting values by startdate')
+# df_complete = merged_df.sort_values(by = ['start_date'])
+# print('df_complete 2 adding merging of weather')
+# df_complete = pd.merge_asof(df_complete, weather, left_on = 'start_date', 
+#                             right_on = 'Datetime', direction = 'nearest').drop('Datetime',  axis=1)
 
-print('convert weather to datetime')
-weather['Datetime'] = pd.to_datetime(weather['Datetime'])
+bixidf2014
 
-#now include weather data with bixi data based on datetime
-print('df_complete 1 sorting values by startdate')
+print('concatenation')
+list_of_pandas = []
+
+# print('df_complete 1.1 sorting values by startdate')
+# df_complete1 = list_of_dataframes[0].sort_values(by = ['start_date'])
+# print('df_complete 1.2 adding merging of weather')
+# df_complete1 = pd.merge_asof(df_complete1, weather, left_on = 'start_date', 
+#                             right_on = 'Datetime', direction = 'nearest').drop('Datetime',  axis=1)
+
+print('bixi2014 size')
+print(bixidf2014.shape)
+print('bixi2015 size')
+print(bixidf2015.shape)
+print('bixi2016 size')
+print(bixidf2016.shape)
+
+list_of_pandas.append(bixidf2014)
+list_of_pandas.append(bixidf2015)
+list_of_pandas.append(bixidf2016)
+
+merged_df = pd.concat(list_of_pandas)
+print('merged_df')
+print(merged_df)
+print(merged_df.shape)
+
+print('df_complete 1.1 sorting values by startdate')
 df_complete = merged_df.sort_values(by = ['start_date'])
-print('df_complete 2 adding merging of weather')
-df_complete = pd.merge_asof(df_complete, weather, left_on = 'start_date', 
+print('df_complete 1.2 adding merging of weather')
+df_complete = pd.merge_asof(df_complete, weather_df, left_on = 'start_date', 
                             right_on = 'Datetime', direction = 'nearest').drop('Datetime',  axis=1)
+
+# list_of_pandas.append(df_complete1)
+
+# print('df_complete 2.1 sorting values by startdate')
+# df_complete2 = bixidf2015.sort_values(by = ['start_date'])
+# print('df_complete 2.2 adding merging of weather')
+# df_complete2 = pd.merge_asof(df_complete2, weather_df, left_on = 'start_date', 
+#                             right_on = 'Datetime', direction = 'nearest').drop('Datetime',  axis=1)
+
+# list_of_pandas.append(df_complete2)
+
+# print('df_complete 3.1 sorting values by startdate')
+# df_complete3 = bixidf2015.sort_values(by = ['start_date'])
+# print('df_complete 3.2 adding merging of weather')
+# df_complete3 = pd.merge_asof(df_complete3, weather_df, left_on = 'start_date', 
+#                             right_on = 'Datetime', direction = 'nearest').drop('Datetime',  axis=1)
+
+# list_of_pandas.append(df_complete3)
+
+# print('df_complete 3.1 sorting values by startdate')
+# df_complete3 = list_of_dataframes[2].sort_values(by = ['start_date'])
+# print('df_complete 3.2 adding merging of weather')
+# df_complete3 = pd.merge_asof(df_complete1, weather, left_on = 'start_date', 
+#                             right_on = 'Datetime', direction = 'nearest').drop('Datetime',  axis=1)
+
+# list_of_pandas.append(df_complete3)
+
+# print('concatenation')
+# list_of_pandas = []
+# # print('appending')
+# list_of_pandas.append(df_complete1)
+# list_of_pandas.append(df_complete2)
+# list_of_pandas.append(df_complete3)
+# # print('merging')
+# merged_df = pd.concat(list_of_pandas)
+print(df_complete.info)
+print(df_complete.dtypes)
 # print(df_complete.info())
 print('all done gg')
 
