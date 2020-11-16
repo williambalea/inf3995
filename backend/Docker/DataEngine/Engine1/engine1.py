19016971ca3d8c4b32805a90ef071fa7425459f8
Dalyna Pak <dalyna.pak@hotmail.fr>
2020-11-15 23:48:10 -0500

    [Dalyna]: get accounts info from db

--- backend/Docker/DataEngine/Engine1/engine1.py
+++ backend/Docker/DataEngine/Engine1/engine1.py
@@ -1,22 +1,25 @@
 import mysql.connector
 from mysql.connector import Error
 import json
-from matplotlib import pyplot as plt
-import pandas as pd
-from flask.json import jsonify
+import logging
+import csv
+import re
 
 class Engine1:
 
     DB_HOSTNAME = "34.70.117.28"
     DB_USERNAME = "root"
     DB_PASSWORD = "jerome"
-    DB_NAMEOFBD = "Bixi"
+    DB_NAMEOFBD = "Server"
     connection = None
-    
+    ACCOUNTS = "SELECT * FROM Accounts"
+    logging.basicConfig(filename='engine1.log', filemode='w', level=logging.INFO, format='[%(asctime)s] %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
+    PATH = "./kaggleData/Stations_2017.csv"
 
     #constructor
     def __init__(self):
         self.connection = self.create_connection(self.DB_HOSTNAME, self.DB_USERNAME, self.DB_PASSWORD, self.DB_NAMEOFBD)
+        # return None
 
     #create connection to DB
     def create_connection(self, host_name, user_name, user_password, db_name):
@@ -28,52 +31,42 @@ class Engine1:
                 passwd=user_password,
                 database=db_name
             )
-            print("Connection to MySQL DB successful", flush=True)
+            logging.info("Connection to MySQL DB successful")
         except Error as e:
-            print(f"The error '{e}' occurred", flush=True)
+            logging.info(f"The error '{e}' occurred")
         return self.connection
 
     #query db then to kind of json with ' instead of "
-    def query_db(self,query, args=(), one=False):
+    def account_db(self):
         myCursor = self.connection.cursor(buffered=True)
-        myCursor.execute(query)
-        r = [dict((myCursor.description[i][0], value) \
-                for i, value in enumerate(row)) for row in myCursor.fetchall()]
-        print("Fetching all data from mySQL", flush=True)
-        return (r[0] if r else None) if one else r
+        myCursor.execute(self.ACCOUNTS)
+        row = myCursor.fetchone()
+        print (row)
+        return self.toJson(row)
+    
+    def getAllStations(self):
+        logging.info("Getting all stations")
+        data = []
+        csvFile = open(self.PATH, 'r', encoding='ISO-8859-1')
+        csvReader = csv.DictReader(csvFile)
+        data = self.toJson([row for row in csvReader])
+        return data
 
-    #query to db and returns pandas object directly
-    def query_pd(self, query):
-        df = pd.read_sql_query(query, self.connection)
-        df['start_date'] = pd.to_datetime(df['start_date'], infer_datetime_format=True)
-        df['end_date'] = pd.to_datetime(df['end_date'], infer_datetime_format=True)
-        print("Fetch data into pandas object", flush=True)
-        return df
-
+    def logsToJSON(self, byte):
+        logs = {}
+        logsFile= open('engine1.log', 'r')
+        logs = logsFile.read()[int(byte):].splitlines()
+        logsFile.close()
+        logs = self.espace_ansi(str(logs))
+        logs = {'logs': logs}
+        return self.toJson(logs)
+        
+    def espace_ansi(self, line):
+        ansi_espace = re.compile(r'(\x9B|\x1B\[)[0-?]*[ -\/]*[@-~]')
+        return ansi_espace.sub(" ", line)
 
     def toJson(self, data):
-        print("Data to JSON dumps", flush=True)
+        logging.info("Data to JSON")
         return json.dumps(data)
-    
-    def jsonToPandas(self, jsonObj):
-        print("JSON to pandas object", flush=True)
-        return pd.read_json(jsonObj)
 
-    #queries 
-    def getStationCode(self, code):
-        query = "SELECT S.name, S.latitude, S.longitude FROM Stations2017 S WHERE code='{}'".format(code)
-        print(query, flush=True)
-        return self.toJson(self.query_db(query))
-        
-    def getAllStations(self):
-        query = "SELECT * FROM Stations2017"
-        print(query, flush=True)
-        return self.toJson(self.query_db(query))
-    
-    def getDataUsage(self,year, station):
-        query = "SELECT * FROM BixiRentals"
-        query += str(year)
-        if station != "all":
-            query += " WHERE startStationCode='{}'".format(station)
-            query += " OR endStationCode='{}' ".format(station)
-        return self.toJson(self.query_db(query))
+
--- backend/Docker/DataEngine/Engine1/engine1_HTTP.py
+++ backend/Docker/DataEngine/Engine1/engine1_HTTP.py
@@ -9,6 +9,10 @@ engine1 = Engine1()
 def hello_world():
     return 'Hello World from Engin 1'
 
+@app.route('/engine1/db')
+def test():
+    return engine1.account_db()
+
 @app.route('/engine1/station/<code>')
 def stationCode(code):
     return engine1.getStationCode(code)
@@ -16,3 +20,7 @@ def stationCode(code):
 @app.route('/engine1/station/all')
 def allStation():
     return engine1.getAllStations() 
+
+@app.route('/engine1/logs/<byte>')
+def logs(byte):
+    return engine1.logsToJSON(byte)
