e549f9483593ff8b170b85ca35ecbf7d13f5eb03
Jean-Michel Lasnier <j-m.lasnier@hotmail.com>
2020-11-15 20:46:06 -0500

    [JML]data is ready for randomForestRegressor

--- backend/Docker/DataEngine/Engine3/engine3.py
+++ backend/Docker/DataEngine/Engine3/engine3.py
@@ -14,22 +14,15 @@ class Engine3:
     def __init__(self):
         return None 
     
-    def importBixiData(self):
-        # csv_file_list = ["../kaggleData/OD_2014.csv", "../kaggleData/OD_2015.csv","../kaggleData/OD_2016.csv"]sv"]
-        list_of_dataframes = []
-        for filename in self.csv_file_list:
-            list_of_dataframes.append(pd.read_csv(filename, dtype={'start_date':str,'start_station_code':int,'end_date':str,
-                                                                'end_station_code':str,'duration_sec':int,'is_member':int})  )
-        return list_of_dataframes
+    def get_bixi_data_year(self, year):
+        path = "../kaggleData/OD_"
+        path += str(year)
+        path += ".csv"
+        bixidf = pd.read_csv(path, dtype={'start_date':str,'start_station_code':int,'end_date':str,
+                                                        'end_station_code':str,'duration_sec':int,'is_member':int})
+        bixidf['start_date'] = pd.to_datetime(bixidf['start_date'])
+        return bixidf
 
-    def convertBixiDataToDatetime(self, list_df):
-        list_df[0]['start_date'] = pd.to_datetime(list_df[0]['start_date'])
-        list_df[0]['end_date'] = pd.to_datetime(list_df[0]['end_date'])
-        list_df[1]['start_date'] = pd.to_datetime(list_df[1]['start_date'])
-        list_df[1]['end_date'] = pd.to_datetime(list_df[1]['end_date'])
-        # list_df[2]['start_date'] = pd.to_datetime(list_df[2]['start_date'])
-        # list_df[2]['end_date'] = pd.to_datetime(list_df[2]['end_date'])
-        return list_df
 
     def get_weather_df(self):
         print('importing weather data')
@@ -57,3 +50,167 @@ class Engine3:
         print(weather.shape)
         return weather
 
+    def merge_bixi_weather_df(self, bixi, weather):
+        print('df_complete 1 sorting values by startdate')
+        df_complete = bixi.sort_values(by = ['start_date'])
+        print('df_complete 2 adding merging of weather')
+        df_complete = pd.merge_asof(df_complete, weather, left_on = 'start_date', 
+                                    right_on = 'Datetime', direction = 'nearest').drop('Datetime',  axis=1)
+        return df_complete
+
+    def get_traning_df(self):
+        print('fetching csv data')
+        bixi2016 = self.get_bixi_data_year(2016)
+        bixi2015 = self.get_bixi_data_year(2015)
+        bixi2014 = self.get_bixi_data_year(2014)
+        weather_df = self.get_weather_df()
+
+        print('concatenation')
+        list_of_pandas = []
+        list_of_pandas.append(bixi2016)
+        list_of_pandas.append(bixi2015)
+        list_of_pandas.append(bixi2014)
+
+        big_bixi_df = pd.concat(list_of_pandas)
+ 
+        print('merging bixi and weather')
+        df = self.merge_bixi_weather_df(big_bixi_df, weather_df)
+        
+        print(df.columns)
+        print('cleaning columns')
+        df.insert(1, "year", df['start_date'].dt.year)
+        df.insert(2, "month", df['start_date'].dt.month)
+        df.insert(3, "day", df['start_date'].dt.day)
+        df.insert(4, "hour", df['start_date'].dt.hour)
+        df.insert(5, "weekday", df['start_date'].dt.weekday)
+        df.insert(6, "weekofyear", df['start_date'].dt.isocalendar().week)
+        df = df.drop( ['start_date',
+                        'end_date',
+                        'end_station_code',
+                        'duration_sec',
+                        'is_member', axis=1)
+        # Group by hour
+        df_grouped = df.groupby(['year','month', 'day', 'hour', 'start_station_code']).agg('first')
+        column = df.groupby(['year','month', 'day', 'hour']).count()['Description']
+        
+        df_grouped['num_trips']= column
+        df_grouped = df_grouped.reset_index()
+        return df_grouped
+
+    def get_testing_df(self):
+        print('fetching csv data')
+        bixi2017 = self.get_bixi_data_year(2017)
+        weather = self.get_weather_df()
+
+        print('merging bixi and weather')
+        df = self.merge_bixi_weather_df(bixi2017, weather)
+
+        print('cleaning columns')
+        df.insert(1, "year", df['start_date'].dt.year)
+        df.insert(2, "month", df['start_date'].dt.month)
+        df.insert(3, "day", df['start_date'].dt.day)
+        df.insert(4, "hour", df['start_date'].dt.hour)
+        df.insert(5, "weekday", df['start_date'].dt.isocalendar().week)
+        df.insert(6, "weekofyear", df['start_date'].dt.weekofyear)
+        df = df.drop( ['start_date',
+                                           'end_date',
+                                           'end_station_code',
+                                           'duration_sec',
+                                           'is_member'], axis=1)
+        # Group by hour
+        df_grouped = df.groupby(['year','month', 'day', 'hour', 'start_station_code']).agg('first')
+        column = df.groupby(['year','month', 'day', 'hour']).count()['Description']
+        
+        df_grouped['num_trips']= column
+        df_grouped = df_grouped.reset_index()
+        
+        return df
+
+
+
+
+#TODELETE
+    # def importBixiData(self):
+    #     # csv_file_list = ["../kaggleData/OD_2014.csv", "../kaggleData/OD_2015.csv","../kaggleData/OD_2016.csv"]sv"]
+    #     list_of_dataframes = []
+    #     for filename in self.csv_file_list:
+    #         list_of_dataframes.append(pd.read_csv(filename, dtype={'start_date':str,'start_station_code':int,'end_date':str,
+    #                                                             'end_station_code':str,'duration_sec':int,'is_member':int})  )
+    #     return list_of_dataframes
+
+#TODELETE
+    # def convertBixiDataToDatetime(self, list_df):
+    #     list_df[0]['start_date'] = pd.to_datetime(list_df[0]['start_date'])
+    #     list_df[0]['end_date'] = pd.to_datetime(list_df[0]['end_date'])
+    #     list_df[1]['start_date'] = pd.to_datetime(list_df[1]['start_date'])
+    #     list_df[1]['end_date'] = pd.to_datetime(list_df[1]['end_date'])
+    #     # list_df[2]['start_date'] = pd.to_datetime(list_df[2]['start_date'])
+    #     # list_df[2]['end_date'] = pd.to_datetime(list_df[2]['end_date'])
+    #     return list_df
+
+
+#TODELETE
+    # def get_bixi_data2014to2016(self):
+    #     # #Import Data
+    #     print('importing bixi csv data')
+    #     bixidf2014 = pd.read_csv("../kaggleData/OD_2014.csv", dtype={'start_date':str,'start_station_code':int,'end_date':str,
+    #                                                             'end_station_code':str,'duration_sec':int,'is_member':int})
+    #     bixidf2015 = pd.read_csv("../kaggleData/OD_2015.csv", dtype={'start_date':str,'start_station_code':int,'end_date':str,
+    #                                                             'end_station_code':str,'duration_sec':int,'is_member':int})
+    #     bixidf2016 = pd.read_csv("../kaggleData/OD_2016.csv", dtype={'start_date':str,'start_station_code':int,'end_date':str,
+    #                                                     'end_station_code':str,'duration_sec':int,'is_member':int})
+
+    #     print('converting bixi data to datetime')
+    #     bixidf2014['start_date'] = pd.to_datetime(bixidf2014['start_date'])
+    #     bixidf2014['end_date'] = pd.to_datetime(bixidf2014['end_date'])
+    #     bixidf2015['start_date'] = pd.to_datetime(bixidf2015['start_date'])
+    #     bixidf2015['end_date'] = pd.to_datetime(bixidf2015['end_date'])
+    #     bixidf2016['start_date'] = pd.to_datetime(bixidf2016['start_date'])
+    #     bixidf2016['end_date'] = pd.to_datetime(bixidf2016['end_date'])
+
+    #     print('concatenation')
+    #     list_of_pandas = []
+    #     list_of_pandas.append(bixidf2014)
+    #     list_of_pandas.append(bixidf2015)
+    #     list_of_pandas.append(bixidf2016)
+
+    #     big_bixi_df = pd.concat(list_of_pandas)
+
+        
+    #     print('merged_df')
+    #     print(big_bixi_df)
+    #     print(big_bixi_df.shape)
+    #     return big_bixi_df
+     
+
+
+    def prep_training_data(self, df):
+        
+        df['start_date'] = pd.to_datetime(df['start_date'])
+        df['weekday'] = df.start_date.dt.dayofweek
+        df['hour'] = df.start_date.dt.hour
+        df['num_week'] = df.start_date.dt.weekofyear
+        df = df.drop(['start_date','start_station_code', 'end_date', 'end_station_code', 'duration_sec', 'is_member', 'Temperature_C'], axis=1)
+        
+        # Group by hour
+        training_df = df.groupby(['num_week','weekday', 'hour']).agg('first')
+        columna = df.groupby(['num_week','weekday', 'hour']).count()['Description']
+        training_df['num_trips']= columna
+        training_df=training_df.reset_index(level=[0,1,2])
+
+        return training_df
+
+
+
+    def testSeparateTime(self):
+        # #Import Data
+        print('importing bixi csv data')
+        bixidf2014 = pd.read_csv("../kaggleData/OD_2014.csv", dtype={'start_date':str,'start_station_code':int,'end_date':str,
+                                                                'end_station_code':str,'duration_sec':int,'is_member':int})
+        
+        print('converting bixi data to datetime')
+        bixidf2014['start_date'] = pd.to_datetime(bixidf2014['start_date'])
+        bixidf2014['end_date'] = pd.to_datetime(bixidf2014['end_date'])
+        bixidf2014.insert(1, "start_month", bixidf2014['start_date'].dt.month)
+
+        return bixidf2014
--- backend/Docker/DataEngine/Engine3/lab3.py
+++ backend/Docker/DataEngine/Engine3/lab3.py
@@ -11,6 +11,7 @@ import numpy as np
 import seaborn as sns
 import base64
 from easydict import EasyDict as edict
+import dask.dataframe as dd
 
 HOST_NAME = "34.70.117.28"
 USER_NAME = "root"
@@ -49,14 +50,7 @@ print('constructing engine3')
 engine3 = Engine3()
 
 # #Import Data
-print('importing bixi csv data')
-# csv_file_list = ["../kaggleData/OD_2014.csv", "../kaggleData/OD_2015.csv","../kaggleData/OD_2016.csv"]
-bixidf2014 = pd.read_csv("../kaggleData/OD_2014.csv", dtype={'start_date':str,'start_station_code':int,'end_date':str,
-                                                        'end_station_code':str,'duration_sec':int,'is_member':int})
-bixidf2015 = pd.read_csv("../kaggleData/OD_2015.csv", dtype={'start_date':str,'start_station_code':int,'end_date':str,
-                                                        'end_station_code':str,'duration_sec':int,'is_member':int})
-bixidf2016 = pd.read_csv("../kaggleData/OD_2016.csv", dtype={'start_date':str,'start_station_code':int,'end_date':str,
-                                                        'end_station_code':str,'duration_sec':int,'is_member':int})
+# print('importing bixi csv data')
 # csv_file_list = ["../kaggleData/OD_2014.csv","../kaggleData/OD_2015.csv", "../kaggleData/OD_2016.csv"]
 # list_of_dataframes = []
 # for filename in csv_file_list:
@@ -65,112 +59,27 @@ bixidf2016 = pd.read_csv("../kaggleData/OD_2016.csv", dtype={'start_date':str,'s
 # list_of_dataframes = []
 # list_of_dataframes = engine3.importBixiData()
 
-print('concatenation bixi data')
-# merged_df = pd.concat(list_of_dataframes)
-# print(list_of_dataframes[0].shape)
-# print(list_of_dataframes[1].shape)
-# print(list_of_dataframes[2].shape)
-# print('shape of merge')
-# print(merged_df.shape)
 
 
-# convert bixi data dateTime
-# print('converting bixi data to datetiime')
-# merged_df['start_date'] = pd.to_datetime(merged_df['start_date'])
-# merged_df['end_date'] = pd.to_datetime(merged_df['end_date'])
-# merged_df[['start_date', 'end_date']].dtypes
-# print('bixiData to Pandas OK' )
-print('converting bixi data to datetiime')
-bixidf2014['start_date'] = pd.to_datetime(bixidf2014['start_date'])
-bixidf2014['end_date'] = pd.to_datetime(bixidf2014['end_date'])
-bixidf2015['start_date'] = pd.to_datetime(bixidf2015['start_date'])
-bixidf2015['end_date'] = pd.to_datetime(bixidf2015['end_date'])
-bixidf2016['start_date'] = pd.to_datetime(bixidf2016['start_date'])
-bixidf2016['end_date'] = pd.to_datetime(bixidf2016['end_date'])
-# list_of_dataframes[0][['start_date', 'end_date']].dtypes
-# list_of_dataframes = engine3.convertBixiDataToDatetime(list_of_dataframes)
-print('bixiData to Pandas OK')
+#################start here#################
+
+#get all data
 
 
-weather_df = engine3.get_weather_df()
+# bigPandas = engine3.merge_bixi_weather_df(bigBixidf, weather_df)
+# training_set = engine3.prep_training_data(bigPandas)
+training_df = engine3.get_traning_df()
+print(training_df)
+print(training_df.columns)
 
-# #now include weather data with bixi data based on datetime
-# print('df_complete 1 sorting values by startdate')
-# df_complete = merged_df.sort_values(by = ['start_date'])
-# print('df_complete 2 adding merging of weather')
-# df_complete = pd.merge_asof(df_complete, weather, left_on = 'start_date', 
-#                             right_on = 'Datetime', direction = 'nearest').drop('Datetime',  axis=1)
-
-bixidf2014
-
-print('concatenation')
-list_of_pandas = []
-
-# print('df_complete 1.1 sorting values by startdate')
-# df_complete1 = list_of_dataframes[0].sort_values(by = ['start_date'])
-# print('df_complete 1.2 adding merging of weather')
-# df_complete1 = pd.merge_asof(df_complete1, weather, left_on = 'start_date', 
-#                             right_on = 'Datetime', direction = 'nearest').drop('Datetime',  axis=1)
-
-print('bixi2014 size')
-print(bixidf2014.shape)
-print('bixi2015 size')
-print(bixidf2015.shape)
-print('bixi2016 size')
-print(bixidf2016.shape)
-
-list_of_pandas.append(bixidf2014)
-list_of_pandas.append(bixidf2015)
-list_of_pandas.append(bixidf2016)
-
-merged_df = pd.concat(list_of_pandas)
-print('merged_df')
-print(merged_df)
-print(merged_df.shape)
-
-print('df_complete 1.1 sorting values by startdate')
-df_complete = merged_df.sort_values(by = ['start_date'])
-print('df_complete 1.2 adding merging of weather')
-df_complete = pd.merge_asof(df_complete, weather_df, left_on = 'start_date', 
-                            right_on = 'Datetime', direction = 'nearest').drop('Datetime',  axis=1)
-
-# list_of_pandas.append(df_complete1)
-
-# print('df_complete 2.1 sorting values by startdate')
-# df_complete2 = bixidf2015.sort_values(by = ['start_date'])
-# print('df_complete 2.2 adding merging of weather')
-# df_complete2 = pd.merge_asof(df_complete2, weather_df, left_on = 'start_date', 
-#                             right_on = 'Datetime', direction = 'nearest').drop('Datetime',  axis=1)
-
-# list_of_pandas.append(df_complete2)
-
-# print('df_complete 3.1 sorting values by startdate')
-# df_complete3 = bixidf2015.sort_values(by = ['start_date'])
-# print('df_complete 3.2 adding merging of weather')
-# df_complete3 = pd.merge_asof(df_complete3, weather_df, left_on = 'start_date', 
-#                             right_on = 'Datetime', direction = 'nearest').drop('Datetime',  axis=1)
-
-# list_of_pandas.append(df_complete3)
-
-# print('df_complete 3.1 sorting values by startdate')
-# df_complete3 = list_of_dataframes[2].sort_values(by = ['start_date'])
-# print('df_complete 3.2 adding merging of weather')
-# df_complete3 = pd.merge_asof(df_complete1, weather, left_on = 'start_date', 
-#                             right_on = 'Datetime', direction = 'nearest').drop('Datetime',  axis=1)
-
-# list_of_pandas.append(df_complete3)
-
-# print('concatenation')
-# list_of_pandas = []
-# # print('appending')
-# list_of_pandas.append(df_complete1)
-# list_of_pandas.append(df_complete2)
-# list_of_pandas.append(df_complete3)
-# # print('merging')
-# merged_df = pd.concat(list_of_pandas)
-print(df_complete.info)
-print(df_complete.dtypes)
-# print(df_complete.info())
-print('all done gg')
+# testing_df = engine3.get_testing_df()
+# print(testing_df)
+# print(testing_df.columns)
 
 
+# print(training_set.info)
+# print(training_set.dtypes)
+# print(training_set.shape)
+
+# # print(df_complete.info())
+# print('all done gg')
