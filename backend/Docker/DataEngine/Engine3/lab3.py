9605eaf06289572d1db576e89be98ff058a0e4e8
Jean-Michel Lasnier <j-m.lasnier@hotmail.com>
2020-11-22 10:07:38 -0800

    [JML]prediction http request DONE

--- backend/Docker/DataEngine/Engine3/lab3.py
+++ backend/Docker/DataEngine/Engine3/lab3.py
@@ -46,18 +46,35 @@ print('constructing engine3')
 engine3 = Engine3()
 
 print('MONTH ******************************8')
-predictions_df = engine3.load_prediction_df()
-filtered_pred_df = engine3.filter_prediction(predictions_df, 6043, perHour, startDate, endDate)
-# print('HOUR ******************************8')
-# predictions_df = engine3.filter_prediction(6043, perHour)
-# print('WEEKDAY ******************************8')
-# predictions_df = engine3.filter_prediction(6043, perWeekDay)
-# print('DATE ******************************8')
-# predictions_df = engine3.filter_prediction(6043, perDate, startDate, endDate)
+# predictions_df = engine3.load_prediction_df()
+# filtered_pred_df = engine3.filter_prediction(predictions_df, 6043, perHour, startDate, endDate)
+# # print('HOUR ******************************8')
+# # predictions_df = engine3.filter_prediction(6043, perHour)
+# # print('WEEKDAY ******************************8')
+# # predictions_df = engine3.filter_prediction(6043, perWeekDay)
+# # print('DATE ******************************8')
+# # predictions_df = engine3.filter_prediction(6043, perDate, startDate, endDate)
 
-x = engine3.get_graph_X(filtered_pred_df, perHour)
+# x = engine3.get_graph_X(filtered_pred_df, perHour)
+# y = engine3.get_graph_Y(filtered_pred_df)
+# pred_graph = engine3.get_prediction_graph( perHour, x, y)
+# forAndroid = engine3.datatoJSON(pred_graph, x, y)
+# print('GG')
+# print(forAndroid)
+station = 5003
+groupby = perMonth
+# startDate = '27-05-2017'
+# endDate = '27-08-2017'
+
+startDate = '27-05-2017'
+endDate = '27-08-2017'
+predictions_df = engine3.load_prediction_df()
+print('predictions_df: ', predictions_df)
+filtered_pred_df = engine3.filter_prediction(predictions_df, station, groupby, startDate, endDate)
+print('filtered_pred_df: ', filtered_pred_df)
+x = engine3.get_graph_X(filtered_pred_df, groupby)
+print('x: ', x)
 y = engine3.get_graph_Y(filtered_pred_df)
-pred_graph = engine3.get_prediction_graph( perHour, x, y)
-forAndroid = engine3.datatoJSON(pred_graph, x, y)
-print('GG')
-print(forAndroid)
+print('y: ', y)
+pred_graph = engine3.get_prediction_graph( groupby, x, y)
+print(engine3.datatoJSON(pred_graph, x, y))
